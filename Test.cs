44096e4e210718c9ed3cd4d9dfc51d777cd9244c
jjvanzon <janjoostvanzon@hotmail.com>
2018-07-28 14:46:17 +0200

    Publish on GitHub.

---
+++ Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VarianceTest
+{
+    public class Events<T> : IEvents<T>
+    {
+        public event Changing<T> Changing;
+        public T Value { get; set; }
+    }
+
+    public interface IEvents<out T>
+    {
+        event Changing<T> Changing;
+        T Value { get; set; }
+    }
+
+    public delegate void Changing<out T>(object sender, IChangingEventArgs<T> e);
+
+    public class ChangingEventArgs<T> : IChangingEventArgs<T>
+    {
+        public T Old { get; }
+        public T Value { get; set; }
+    }
+
+    public interface IChangingEventArgs<out T>
+    {
+        T Old { get; }
+        T Value { get; set; }
+    }
+}
